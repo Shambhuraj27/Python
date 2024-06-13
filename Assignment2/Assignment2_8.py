dabddb2e0d38a439374c060b9d2c1e5c198f939d
Shambhuraj Patil <127391185+Shambhuraj27@users.noreply.github.com>
2024-06-13 14:04:54 +0530

    Assignment2_8.py

--- Assignment2/Assignment2_8.py
+++ Assignment2/Assignment2_8.py
@@ -5,7 +5,6 @@ def main():
     for i in range(1,No+1):
         for j in range(1,i+1):
             print(j,end = " ")
-        print("\n")
-    
+        print("\n")  
 if __name__ == "__main__":
     main()
